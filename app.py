59789c686c4677073be8962bb0669f6180dcb75c
Parker Brooks <jonparkerbrooks@gmail.com>
2024-02-21 19:29:52 -0600

    styled the navigation bar

--- app.py
+++ app.py
@@ -10,5 +10,22 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/list-stocks")
+def list_stocks():
+    return render_template("list-stocks.html")
+
+@app.route('/add-stock')
+def add_stock():
+    return render_template('add-stock.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/logout')
+def logout():
+    return "you cant even log in tho <a href='/'>home</a>"
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=9001)
