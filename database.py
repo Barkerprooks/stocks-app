f088de79126630ee7c82f13c77dbf0c7ff9a3740
Parker Brooks <jonparkerbrooks@gmail.com>
2024-04-03 19:30:17 -0500

    started on add stocks

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import sqlite3
+import datetime
 
 def initialize_database():
     db = sqlite3.connect("stocks.db")
@@ -9,16 +10,37 @@ def initialize_database():
                     purchase_price REAL,
                     date NUMBER,
                     share_price REAL,
-                    stock_position NUMBER )''')
+                    stock_position NUMBER 
+                    userid NUMBER )''')
+
 
 def create_user(db, username, password):
     db.execute("INSERT INTO users VALUES (?, ?)", [username, password])
     db.commit()
 
+
 def check_password(db, username, password):
     result = db.execute("SELECT password FROM users WHERE username = ?", [username])
     stored_password = result.fetchone()[0]
     return stored_password == password
+
+
+def create_stock(db, symbol, shares, purchase_price, share_price, stock_position, userid):
+    date = datetime.date.today()
+    
+    fields = [ 
+        symbol, 
+        shares, 
+        purchase_price, 
+        date.strftime('%D'), 
+        share_price, 
+        stock_position,
+        userid
+    ]
+
+    db.execute("INSERT INTO stocks VALUES (?, ?, ?, ?, ?, ?, ?)", fields)
+    db.commit()
+
 
 if __name__ == "__main__":
     initialize_database()
